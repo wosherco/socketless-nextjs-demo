ae99fdc09d4a43956a441628909cd9f487e2b4e7
polvallverdu <pvallverdualcaide@gmail.com>
2024-09-06 00:28:27 +0200

    Fixed api route for cloudflare pages

--- src/app/api/socketless/route.ts
+++ src/app/api/socketless/route.ts
@@ -2,4 +2,10 @@ import { socketless } from "@/server/socketless";
 
 export const runtime = "edge";
 
-export const { POST } = socketless().generateRoutes();
+/// In a normal environment, you would use the following code
+// export const { POST } = socketless.generateRoutes();
+
+/// Since we are using Cloudflare Workers, we need to use the following code instead to be able to get the request context
+export function POST(request: Request) {
+  return socketless().generateRoutes().POST(request);
+}
